063846cab5b5bfd00e37851b360e6fad3a0eed2f
Oka Sigit <oka.sigit9@gmail.com>
2023-08-12 15:13:54 +0700

    improvement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div id="" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         @foreach ($banner as $index => $item)
-            <div class="carousel-item @if ($index === 0) active @endif">
+            <div class="carousel-item @if ($index === 0) active @endif s-{{imageDir()}}">
             <img src="{{ imageDir().'banner/'.$item->file_name}}" class="d-block w-100" alt="{{$item->file_name}}">
           </div>
         @endforeach
